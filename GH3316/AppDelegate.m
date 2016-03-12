327708872013d2a09eca64f68ec3cfa5542ca249
JP Simard <jp@jpsim.com>
2016-03-11 17:30:23 -0800

    try to reproduce issue reported at https://github.com/realm/realm-cocoa/issues/3316

--- GH3316/AppDelegate.m
+++ GH3316/AppDelegate.m
@@ -7,12 +7,45 @@
 //
 
 #import "AppDelegate.h"
+#import <Realm/Realm.h>
+
+NSInteger const kDatabaseVersion = 1;
+
+@interface Pair : RLMObject
+@property int left;
+@property int right;
+@end
+@implementation Pair
+@end
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    [self configureDatabase];
+
+    RLMRealm *realm = [RLMRealm defaultRealm];
+    [realm beginWriteTransaction];
+    Pair *pair = [[Pair alloc] init];
+    pair.left = 0;
+    pair.right = 1;
+    [realm addObject:pair];
+    [realm commitWriteTransaction];
+
+    NSLog(@"all pairs: %@", [Pair allObjects]);
     return YES;
+}
+
+- (void)configureDatabase {
+    RLMRealmConfiguration *config = [RLMRealmConfiguration defaultConfiguration];
+    config.schemaVersion = kDatabaseVersion;
+    config.migrationBlock = ^(RLMMigration *migration, uint64_t oldSchemaVersion) {
+
+        if (oldSchemaVersion < kDatabaseVersion) {}
+    };
+
+    [RLMRealmConfiguration setDefaultConfiguration:config];
+
+    [RLMRealm defaultRealm];
 }
 
 @end
